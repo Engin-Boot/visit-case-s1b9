9ce42e224e24aa9bbaae0559475d3a9f5ade02c6
sriyasrini23 <sriyasrini238@gmail.com>
2020-09-11 22:23:05 +0400

    Corrected_file_path

--- Sender/fileReader-test.cpp
+++ Sender/fileReader-test.cpp
@@ -6,6 +6,6 @@ TEST_CASE("TwoArg Constructor")
 {	FileReader reader1("random");
 	REQUIRE((reader1.GetFileName()).compare("SafeFile.csv") == 0);
 
-	FileReader reader2("../test-data/input.csv");
-	REQUIRE((reader2.GetFileName()).compare("../test-data/input.csv") == 0);
+	FileReader reader2("./test-data/input.csv");
+	REQUIRE((reader2.GetFileName()).compare("./test-data/input.csv") == 0);
 }
