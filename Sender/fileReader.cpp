71fa5dc1cacfafdc4a87f7272fac427fcadb0cc0
sriyasrini23 <sriyasrini238@gmail.com>
2020-09-11 23:21:44 +0400

    Added_Reader_to_FileReader_and_corrected_csv_file

--- Sender/fileReader.cpp
+++ Sender/fileReader.cpp
@@ -1,11 +1,31 @@
 #include"fileReader.h"
 
-FileReader::FileReader(std::string filePathName):_fileName("SafeFile.csv")
+FileReader::FileReader(std::string filePathName):_fileName("./Sender/SafeFile.csv")
 {
 	_filePtr.open(filePathName);
 	if (_filePtr.good())
 		_fileName = filePathName;
-	else
-		_filePtr.open(_fileName);
+	
+	_filePtr.close();
+	_filePtr.open(_fileName);
 }
+
 std::string FileReader::GetFileName() { return _fileName; }
+
+bool FileReader::ReadFile()
+{
+	if (_filePtr.good())
+	{
+		while (!_filePtr.eof())
+		{
+			std::string line;
+			getline(_filePtr, line, '\n');
+			_fileLines.push_back(line);
+		}
+		return true;
+	}
+	return false;
+}
+bool FileReader::ReadFileIsGood() { return ReadFile(); }
+
+std::vector<std::string> FileReader::GetFileLines() { return _fileLines; }
--- Sender/fileReader.h
+++ Sender/fileReader.h
@@ -8,8 +8,13 @@ private:
 	std::string _fileName;
 	std::ifstream _filePtr;
 
-	std::vector<std::string> _fileLine;
+	std::vector<std::string> _fileLines;
+
+	bool ReadFile();
+
 public:
 	FileReader(std::string);
 	std::string GetFileName();
+	std::vector<std::string> GetFileLines();
+	bool ReadFileIsGood();
 };
