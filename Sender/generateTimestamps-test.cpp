22407ef7687bd532d08f1e1bfe3a00e6d9df0933
sriyasrini23 <sriyasrini238@gmail.com>
2020-09-13 19:20:58 +0400

    test - complexity

--- Sender/generateTimestamps-test.cpp
+++ Sender/generateTimestamps-test.cpp
@@ -16,11 +16,15 @@ TEST_CASE("GenerateRandomNumbers-range")
 {
 	int size = 5;
 	std::vector<int> numbers = generateRandomNumbers(size);
-	REQUIRE(((numbers[0] >= 0) && (numbers[0] <= 1440))==true);
-	REQUIRE(((numbers[1] >= 0) && (numbers[1] <= 1440))==true);
-	REQUIRE(((numbers[2] >= 0) && (numbers[2] <= 1440))==true);
-	REQUIRE(((numbers[3] >= 0) && (numbers[3] <= 1440))==true);
-	REQUIRE(((numbers[4] >= 0) && (numbers[4] <= 1440))==true);
+	for (int i = 0; i < 5; i++)
+	{
+		REQUIRE(((numbers[i] >= 0) && (numbers[i] <= 1440)) == true);
+	}
+	//REQUIRE(((numbers[0] >= 0) && (numbers[0] <= 1440))==true);
+	//REQUIRE(((numbers[1] >= 0) && (numbers[1] <= 1440))==true);
+	//REQUIRE(((numbers[2] >= 0) && (numbers[2] <= 1440))==true);
+	//REQUIRE(((numbers[3] >= 0) && (numbers[3] <= 1440))==true);
+	//REQUIRE(((numbers[4] >= 0) && (numbers[4] <= 1440))==true);
 }
 
 TEST_CASE("GenerateRandomNumbers-sorted")
