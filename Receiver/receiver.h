cca91d7c5f639be0f18d610e17a92e24abdce45f
Shubham <Shubham.gaikwad88.15@gmail.com>
2020-09-16 09:05:59 +0530

    Update receiver.h

--- Receiver/receiver.h
+++ Receiver/receiver.h
@@ -6,8 +6,8 @@ class Receiver
 private:
 	int hours;
 	int days;
-	int peakValue=0;
-	int peakdate=0;
+	int peakValue = 0;
+	int peakdate = 0;
 	std::vector<Data> BufferFootfall;
 	std::vector<std::vector<int>> visitCount;
 	std::vector<float> hourlyAvgFootfall;
@@ -15,27 +15,36 @@ private:
 
 public:
 	Receiver();
-	Receiver(int,int);
+	Receiver(int, int);
+
+	int getDays();
+	int getHours();
+
+	int getPeekValue();
+	int getPeakDateValue();
+
+	std::vector<Data> getBuffer();
+
+	std::vector<std::vector<int>> getVisitCount();
+	void setVisitCount(int, int, int);
+
+	std::vector<float> gethourAvg();
+	std::vector<float> getDailyAvg();
 
 	void getSenderData();
 	void storeInBuffer(std::string&, std::string&);
-	
+
 	void clearBuffer();
 	void storeCountInVector();
 
-	
-	
-	void storeAvgForPreviousMonth();
-
 	void getAvgHourlyfootfall();
-	void getAvgHourlyfootfall(int,int);
+	void getAvgHourlyfootfall(int, int);
 	void getAvgDailyfootfall();
 	int getPeekDailyfootfallMontly();
 
 	void StoreInCSV();
-
 };
 
-std::vector<int> getMonthandYear();
+std::vector<int> getMonthandYear(std::string);
 int checkMonth(int);
 void split(std::vector<std::string>&, std::string, char);
